872a2a635fcaab957c6cecf296d86470a08fff68
Syed Huzaifa <imsyed1998@gmail.com>
2024-10-02 12:14:42 +0500

    Only Admin can access Permissions APIs

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express();
 const auth = require('../middlewares/authMiddleware');
 const permissionController = require('../controllers/admin/permissionConstroller');
-const { permissionAddValidator, permissionDeleteValidator ,permissionUpdateValidator} = require('../helpers/adminValidator');
+const { permissionAddValidator, permissionDeleteValidator, permissionUpdateValidator } = require('../helpers/adminValidator');
+const { onlyAdminAccess } = require('../middlewares/adminMiddleware');
 
 // below is 'registerValidator' is middleware used apply on req
 
 router.post('/add-permission', auth, permissionAddValidator, permissionController.addPermission);
-router.get('/get-permission', auth, permissionController.getPermission);
+router.get('/get-permission', auth, onlyAdminAccess, permissionController.getPermission);
 router.post('/delete-permission', auth, permissionDeleteValidator, permissionController.deletePermission);
 router.post('/update-permission', auth, permissionUpdateValidator, permissionController.updatePermission);
 
