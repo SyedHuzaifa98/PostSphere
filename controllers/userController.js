ff08b0be59b642bab7468034efa0bcb652710da1
Syed Huzaifa <imsyed1998@gmail.com>
2024-10-08 15:13:14 +0500

    Send Mail to User with their Details & Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const User = require('../models/userModel');
+const { sendMail } = require('../helpers/mailer');
 const bcrypt = require('bcrypt');
 const randomstring = require('randomstring');
 const createUser = async (req, res) => {
@@ -43,6 +44,26 @@ const createUser = async (req, res) => {
         const user = User(obj);
         const userData = await user.save();
         console.log(password);
+        const content = `
+        <p>Hii <b>`+ userData.name + `</b> Your account is created , below is your details.</p>
+        <table style="border-style:none;">
+            <tr>
+                <th>Name: </th>
+                <td>`+ userData.name + `</td>
+            </tr>
+            <tr>
+                <th>Email: </th>
+                <td>`+ userData.email + `</td>
+            </tr>
+            <tr>
+                <th>Password: </th>
+                <td>`+ password + `</td>
+            </tr>
+        </table>
+        <p> Now you can login your account in our application, Thanks..... </p>
+        `;
+        sendMail(userData.email, 'Account Created', content);
+
         return res.status(200).json({
             success: true,
             msg: 'User created successfully!',
